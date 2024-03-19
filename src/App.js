eb99182068df11f9818591e959e203afc8882c1c
Palesa-Tsotetsi <152623456+Palesa-Tsotetsi@users.noreply.github.com>
2024-03-19 15:05:36 +0200

    fixed target error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Weather defaultCity="Cape Town"/>
     
 
-    <footer> This project was coded by <a href="https://github.com/Palesa-Tsotetsi/react-weather-app-search-engine.git" target="_blank">Palesa Tsotetsi</a> and can be found on <a href="https://github.com/Palesa-Tsotetsi/react-weather-app-search-engine.git" target="_blank">GitHub.</a> </footer>
+    <footer> This project was coded by <a href="https://github.com/Palesa-Tsotetsi/react-weather-app-search-engine.git" target="_blank" rel="noreferrer">Palesa Tsotetsi</a> and can be found on <a href="https://github.com/Palesa-Tsotetsi/react-weather-app-search-engine.git" target="_blank" rel="noreferrer">GitHub.</a> </footer>
     </div>
   );
 }
